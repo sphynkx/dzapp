11184524005ac9e39b334106c2fd68b480603216
sphynkx <sfynkx@gmail.com>
2024-11-28 08:41:11 +0200

    Add time fields

--- dzencode/models.py
+++ dzencode/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone  # для получения текущего времени
 
 class User(models.Model):
     username = models.CharField(max_length=50)
@@ -7,4 +8,5 @@ class MsgDb(models.Model):
     title = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
     published_date = models.DateField()
+    published_time = models.TimeField(default=timezone.now)  # новое поле для времени
     content = models.TextField()
--- dzencode/views.py
+++ dzencode/views.py
@@ -3,23 +3,21 @@ from django.http import JsonResponse
 from .models import MsgDb
 
 def index(request):
-    hellow = "Some"
-    word = 'Vars'
-    title = request.GET.get('title', 'default_title')
-
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        title = request.GET.get('title', 'default_title')
         if MsgDb.objects.filter(title=title).exists():
             msg = MsgDb.objects.get(title=title)
             data = {
                 'message': msg.content,
                 'title': msg.title,
                 'user_name': msg.user.username,
-                'date': msg.published_date
+                'date': msg.published_date,
+                'time': msg.published_time  # добавляем время
             }
             return JsonResponse(data)
         else:
             return JsonResponse({'error': 'Message does not exist'}, status=404)
     else:
         msgs = MsgDb.objects.select_related('user').all()
-        return render(request, 'index.html', context={'some': hellow, 'vars': word, 'msgs': msgs})
+        return render(request, 'index.html', context={'msgs': msgs})
 
